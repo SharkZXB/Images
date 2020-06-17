568452edc5666efb04edc7ca0d7533ba01a0fb43
zhangxinbing <229153959@qq.com>
2020-06-17 10:43:23 +0800

    change

---
+++ glide/src/main/java/com/sharkz/glide/document/GlideDocument.java
@@ -0,0 +1,20 @@
+package com.sharkz.glide.document;
+
+
+
+/**
+ * ================================================
+ * 作    者：SharkZ
+ * 邮    箱：229153959@qq.com
+ * 创建日期：2020/5/20  13:32
+ * 描    述 https://blog.csdn.net/asd501823206/article/details/106074039/
+ * 修订历史：
+ * ================================================
+ */
+public class GlideDocument {
+
+    private void test(){
+
+    }
+
+}
